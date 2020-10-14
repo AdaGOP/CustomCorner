ae29066bd9931bd321c406b8dae16e2868c3e5cb
zeinrezky <zeinrezky@gmail.com>
2020-10-14 10:17:57 +0700

    Initial Commit

---
+++ TestPackage/TestPackage.h
@@ -0,0 +1,19 @@
+//
+//  TestPackage.h
+//  TestPackage
+//
+//  Created by zein rezky chandra on 14/10/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestPackage.
+FOUNDATION_EXPORT double TestPackageVersionNumber;
+
+//! Project version string for TestPackage.
+FOUNDATION_EXPORT const unsigned char TestPackageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPackage/PublicHeader.h>
+
+
